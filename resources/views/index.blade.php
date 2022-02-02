82a0458e91554b89e99a7687c7cbd4ae7cc92a96
francesco <francescosanti91@gmail.com>
2022-02-02 17:07:44 +0100

    aggiunti form in index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h3>Registrati</h3>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="username"><br>
+        <input type="email" name="email" placeholder="la tua email"><br>
+        <input type="password" name="password" placeholder="la tua password"><br>
+        <input type="password" name="password_confirmation" placeholder="ripeti la password"><br><br>
+        <input type="submit" value="Registrati">
+    </form>
+
+    <h3>oppure accedi</h3>
+    <form action="" method="post">
+        <input type="email" name="email" placeholder="la tua email"><br>
+        <input type="password" name="password" placeholder="la tua password"><br><br>
+        <input type="submit" value="Accedi">
+    </form>
+</body>
+</html>
